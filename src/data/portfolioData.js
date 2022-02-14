52bed05c77e331b0b19fbd47032487567fa039a6
CorentinCrt <corentin.carette80@gmail.com>
2022-02-14 11:34:08 +0100

    ajout Portfolio

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -6,7 +6,7 @@ export const portfolioData = [
     languagesIcons: ["fab fa-js", "fab fa-css3-alt", "fab fa-react"],
     source: "https://gitlab.com/corentin.carette80/cv-corentincrt",
     info: "CV que vous êtes actuellement entrain de regarder",
-    picture: "./media/project1.JPG",
+    picture: "./media/project1.png",
   },
   {
     id: 2,
@@ -16,6 +16,15 @@ export const portfolioData = [
     source: "https://gitlab.com/corentin.carette80/api-react-film",
     info: "Utilisation de l'api moviedb pour rechercher un film et l'ajouter à ses favoris en React",
     picture: "./media/project2.JPG",
+  },
+  {
+    id: 3,
+    name: "Site vitrine aérodrome",
+    languages: ["javascript", "css"],
+    languagesIcons: ["fab fa-js", "fab fa-css3-alt"],
+    source: "https://gitlab.com/corentin.carette80/acsq-aero-club",
+    info: "Site vitrine pour un aérodrome fait lors de mon stage de L2 (non publier)",
+    picture: "./media/project3.png",
   },
   //"php" =>"fab fa-php"
   //"node" =>"fab fa-node"
